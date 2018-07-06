46eb823494fe2ee662a3af9401d449a69754440f
Andrey Nakin <andrey@ter-net.com>
2018-07-06 14:30:28 +0300

    work

--- isnp.file/R/exp.data.R
+++ isnp.file/R/exp.data.R
@@ -1,6 +1,21 @@
 # return a list that encapsulates given data frame with experiment properties
 exp.data <-
   function(basedir = ".", data) {
+    
+    get.value <- function(props, propname) {
+      v <- props[[propname]]
+      if (is.null(v)) {
+        return (NA)
+      } else {
+        num <- suppressWarnings(as.numeric(v))
+        if (is.na(num)) {
+          return (v)
+        } else {
+          return (num)
+        }
+      }
+    }
+
     data <- list(
       data = data, 
       expProps = exp.props(basedir), 
@@ -9,5 +24,22 @@ exp.data <-
       numOfRuns = length(data),
       numOfSeries = max(sapply(data, function(run.data) length(run.data)))
     )
+    
+    # collect experiment-level props
+    if (!is.null(data$expProps)) {
+      for (i in names(data$expProps)) {
+        data[[i]] <- get.value(data$expProps, i)
+      }
+        
+    }
+    
+    # collect run-level props
+    if (!is.null(data$runProps) && data$numOfRuns > 0) {
+      props <- list()
+      for (runNo in seq(1, data$numOfRuns, 1)) {
+        
+      }
+    }
+    
     return (data)
   }
